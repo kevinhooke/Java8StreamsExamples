4ddd5544680a49aac4f6e22e2b4eb4679d51b26a
kevinhooke <kevin.hooke@gmail.com>
2021-01-30 10:51:16 -0800

    test update

--- src/test/java/kh/streams/StreamsWithListsTest.java
+++ src/test/java/kh/streams/StreamsWithListsTest.java
@@ -1,5 +1,8 @@
 package kh.streams;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,16 +10,15 @@ import org.junit.Test;
 
 public class StreamsWithListsTest {
 
-    private static final List<Integer> list1 = Arrays.asList(1, 3, 5, 7, 3, 5, 7, 2);
+    private static final List<Integer> LIST1 = Arrays.asList(1, 3, 4, 5, 7, 3, 5, 7, 2);
+    private static final List<Integer> EXPECTED_RESULT1 = Arrays.asList(4, 2);
+    
     
     @Test
     public void testFilterListEvenValues() {
         
-        List<Integer> result = new StreamsWithLists().filterListEvenValues(list1);
-        
-        //TODO assert
-        
-        
-        
+        List<Integer> result = new StreamsWithLists().filterListEvenValues(LIST1);
+        assertEquals(2, result.size());
+        assertTrue(result.containsAll(EXPECTED_RESULT1));
     }
 }
