7cb48a687e88992db7e877e4224e1a0b46167ba5
kevinhooke <kevin.hooke@gmail.com>
2021-01-31 14:43:46 -0800

    additional examples and tests

--- src/main/java/kh/streams/StreamsWithLists.java
+++ src/main/java/kh/streams/StreamsWithLists.java
@@ -1,7 +1,11 @@
 package kh.streams;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class StreamsWithLists {
 
@@ -11,4 +15,60 @@ public class StreamsWithLists {
                 .filter(item -> item % 2 == 0)
                 .collect(Collectors.toList());
     }
+    
+    public Integer getMaxValue(List<Integer> list){
+        
+        return list.stream()
+                .max(Integer::compare).get();
+    }
+
+    public Integer getMinValue(List<Integer> list){
+        
+        return list.stream()
+                .min(Integer::compare).get();
+    }
+    
+    /**
+     * Counts occurrences of each element in the list using Collectors.groupingBy()
+     * to group by each input element and count each group.
+     * 
+     * @param list
+     * @return Map of counts for each input element
+     */
+    public Map<Integer, Long> countOccurrencesInList(List<Integer> list){
+        
+        return list.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
+
+    /**
+     * Takes a nested list of lists and flatmaps the result to a single list.
+     * 
+     * @param list
+     * @return
+     */
+    public List<Integer> flatMapNestedLists(List<List<Integer>> list){
+        
+        return list.stream()
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+    }
+    
+    /**
+     * Given a list of list of ints, find the lists where a digit exists
+     */
+    public List<Integer> findIndexOfListWhereValueExists(int value, List<List<Integer>> list){
+        return IntStream.range(0, list.size())
+                .filter(i -> list.get(i).contains(Integer.valueOf(value)))
+                .mapToObj(Integer::valueOf)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Given a list of list of ints, find the list where each value in a list of ints exists
+     */
+//    public List<Integer> findIndexOfListWhereValuesExist(){
+//        
+//    }
+
 }
--- src/test/java/kh/streams/StreamsWithListsTest.java
+++ src/test/java/kh/streams/StreamsWithListsTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
 
@@ -13,6 +14,9 @@ public class StreamsWithListsTest {
     private static final List<Integer> LIST1 = Arrays.asList(1, 3, 4, 5, 7, 3, 5, 7, 2);
     private static final List<Integer> EXPECTED_RESULT1 = Arrays.asList(4, 2);
     
+    private static final List<List<Integer>> NESTED_LIST1 = Arrays.asList( Arrays.asList(1, 2),
+            Arrays.asList(3, 4), Arrays.asList(5, 6), Arrays.asList(3, 4));
+    private static final List<Integer> EXPECTED_RESULT2 = Arrays.asList(1, 2, 3, 4, 5, 6, 3, 4);
     
     @Test
     public void testFilterListEvenValues() {
@@ -20,5 +24,54 @@ public class StreamsWithListsTest {
         List<Integer> result = new StreamsWithLists().filterListEvenValues(LIST1);
         assertEquals(2, result.size());
         assertTrue(result.containsAll(EXPECTED_RESULT1));
+    }
+
+    @Test
+    public void testGetMinValue() {
+        
+        Integer result = new StreamsWithLists().getMinValue(LIST1);
+        assertEquals(1, result.intValue());
+    }
+
+    @Test
+    public void testGetMaxValue() {
+        
+        Integer result = new StreamsWithLists().getMaxValue(LIST1);
+        assertEquals(7, result.intValue());
+    }
+
+    @Test
+    public void testCountOccurrencesInList() {
+        
+        Map<Integer, Long> result = new StreamsWithLists().countOccurrencesInList(LIST1);
+        assertEquals(1, result.get(1).longValue());
+        assertEquals(1, result.get(2).longValue());
+        assertEquals(2, result.get(3).longValue());
+        assertEquals(1, result.get(4).longValue());
+        assertEquals(2, result.get(5).longValue());
+        assertEquals(null, result.get(6));
+    }
+    
+    @Test
+    public void testflatMapNestedLists(){
+        List<Integer> result = new StreamsWithLists().flatMapNestedLists(NESTED_LIST1);
+        assertTrue(result.containsAll(EXPECTED_RESULT2));
+        assertEquals(1, result.get(0).intValue());
+        assertEquals(6, result.get(5).intValue());
+    }
+    
+    
+    @Test
+    public void testFindIndexOfListWhereValueExists() {
+        List<Integer> result = new StreamsWithLists().findIndexOfListWhereValueExists(3, NESTED_LIST1);
+        assertEquals(2, result.size());
+        assertEquals(1, result.get(0).intValue());
+        assertEquals(3, result.get(1).intValue());
+    }
+    
+    @Test
+    public void testFindIndexOfListWhereValueExists_notInLists() {
+        List<Integer> result = new StreamsWithLists().findIndexOfListWhereValueExists(7, NESTED_LIST1);
+        assertEquals(0, result.size());
     }
 }
